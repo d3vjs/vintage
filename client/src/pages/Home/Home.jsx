81faed3723b203b546c00d7dc3ce2745cd7c2626
David <stankovicdaviddev@gmail.com>
2021-09-28 09:56:30 +0200

    Initial commit; Setup tailwind, styled components, fonts and folder structure

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar';
+import { StyledDiv } from './HomeStyles';
+
+const Home = () => {
+    return (
+        <StyledDiv>
+            <Navbar />
+        </StyledDiv>
+    )
+}
+
+export default Home
---
+++ client/src/pages/Home/HomeStyles.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const StyledDiv = styled.div`
+    ${tw`bg-red-400 text-white`}
+`;
