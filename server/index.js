d9052bac48661aac4cfdd51a4d89598548124699
David <stankovicdaviddev@gmail.com>
2021-10-04 19:55:10 +0200

    Adding Stripe payments to the backend API

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const authRoute = require('./routes/auth');
 const productsRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const ordersRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
 const mongoose = require('mongoose');
 
@@ -20,12 +22,14 @@ mongoose.connect(process.env.MONGO_URL)
         console.log(err);
     });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", ordersRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Backend is running!');
